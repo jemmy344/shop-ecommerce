6a8b81b18b9e358d3d2687b9c2b8498f5b6bdff0
Jonatan Kwiatkowski <joniqs91@gmail.com>
2023-02-08 17:39:28 +0000

    Added fully working checkout page and payments page. Consolidated styles, added extra polish.

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -11,12 +11,15 @@
         if (form.checkValidity() === false) {
           event.preventDefault()
           event.stopPropagation()
+          setformtolocalStorage()
         }
         form.classList.add('was-validated')
       }, false)
     })
   }, false)
 })()
+
+//$('#paymentBtn').on('click', setformtolocalStorage());
 
 //Get local storage and append it to list
 // Function that gets player scores from localStorage and displays them
@@ -34,16 +37,36 @@ function getItems() {
       // For each sorted element
       var sumBasket = 0;
       for(var i = 0; i < sortedArray.length; i++) { 
+        sumBasket = sumBasket + basket[i].price;
         var item = 
         `<li class="list-group-item d-flex justify-content-between lh-condensed">
             <div>
               <h6 class="my-0">${basket[i].product}</h6>
             </div>
-            <span class="text-muted">${basket[i].price}</span>
+            <span class="text-muted">£${basket[i].price.toFixed(2)}</span>
           </li>`;
         $('.list-group').append(item);   
       }
+      var total = 
+          `<li class="list-group-item d-flex justify-content-between lh-condensed">
+            <div>
+              <h6 class="my-0">Total (GBP)</h6>
+            </div>
+            <span class="text-muted">£${sumBasket.toFixed(2)}</span>
+          </li>`;
+          $('.list-group').append(total);   
   }
 }
+
+function setformtolocalStorage() {
+    // Create a basket item
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var email = $('#email').val();
+
+    localStorage.setItem('name', firstName);
+    localStorage.setItem('lastName', lastName);
+    localStorage.setItem('email', email);
+  }
 
 getItems();
