593595ec3fcebdb3f0d39f3ee7845eee6dd88c3f
Liam <liamjwatson@aol.com>
2023-02-08 13:42:23 +0000

    fiexes bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-var queryURL = "https://fakestoreapi.com/products";
 function getConverter() {
   var queryconverterURL =
     "https://api.api-ninjas.com/v1/convertcurrency?want=EUR&have=USD&amount=5000";
@@ -9,7 +8,7 @@ function getConverter() {
     contentType: "application/json",
     // Show result from converter api
     success: function (result) {
-      console.log(result);
+      // console.log(result);
     },
     // Show errors
     error: function ajaxError(jqXHR) {
@@ -17,83 +16,102 @@ function getConverter() {
     },
   });
 }
+
+//  AJAX CALL TO GET ALL PRODUCTS
 function getAllProducts() {
+  var queryURL = "https://fakestoreapi.com/products/";
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function (products) {
-    $(".product-container").append("<div class='row'>");
-
-    $(".row").each(function () {
-      for (var i = 0; i < products.length; i++) {
-        // change 9am to 09am for string comparison
-        $(".row").append(
-          `<div class="card-container col-md-4 col-sm-6">
-              <div class="card h-100">
-                <img class="product-img" src=${
-                  products[i].image
-                } alt="image cap"/>
-                <div class="card-body d-flex flex-column">
-                  <h3 class='card-text card-title product-title'>${
-                    products[i].title
-                  }</h3>
-                  <p class= "card-text price product-price"> £${products[
-                    i
-                  ].price.toFixed(2)}</p>
-                  <p class= "card-text">${products[i].description}</p>
-                  <button type='button' class=' mt-auto btn-success btn add-to-basket'>Add to basket</button>
-                </div>
-              </div>
-           </div>`
-        );
-      }
-    });
-
-    $(".product-img").css({
-      margin: "10% auto",
-      height: "50%",
-      width: "60%",
-      "object-fit": "fit",
-    });
-
-    $(".card").css({ margin: "3%", "border-bottom": "none" });
-
-    $(".btn").css({ width: "70%", margin: "0 auto" });
-
-    $(".add-to-basket").on("click", function () {
-      var productTitle = $(this).siblings(".product-title").text();
-      var productPrice = $(this).siblings(".product-price").text();
-      addtoBasket(productTitle, productPrice);
-    });
-    console.log(localStorage);
+  }).then(function (productData) {
+    // DISPLAY THEM ON PAGE
+    displayProductCards(productData);
+    // create the searchbar
+    createSearchBar();
+    // send api product data to searchbar, to be compared to the user search input
+    searchInput(productData);
   });
-
-  // Function to add the product to a basket, and send to localStorage
-  function addtoBasket(productTitle, productPrice) {
-    // Create a basket
-    var basketArray = [];
-    // Create a basket item
-    var basketItem = {
-      product: productTitle,
-      price: productPrice,
-    };
-
-    // Create my object that we push to localStorage
-    // If a basket is empty, it won't be in local storage
-    if (localStorage.getItem("basket") === null) {
-      // put the item in the basket
-      basketArray.push(basketItem);
-      // send the basket to localStorage
-      localStorage.setItem("basket", JSON.stringify(basketArray));
-    } else {
-      //  the basket is already in localStorage, basketArray becomes the basket in the localStorage using parse
-      basketArray = JSON.parse(localStorage.getItem("basket"));
-      // put the new item in the basket
-      basketArray.push(basketItem);
-      // return the basket to localStorage
-      localStorage.setItem("basket", JSON.stringify(basketArray));
-    }
-  }
 }
 getAllProducts();
 
+// Displays the product information on product cards
+function displayProductCards(products) {
+  $(".product-container").append("<div class='row'>");
+  $(".row").each(function () {
+    for (var i = 0; i < products.length; i++) {
+      $(".row").append(
+        `<div class="card-container col-md-4 col-sm-6">
+            <div class="card h-100">
+              <img class="product-img" src=${
+                products[i].image
+              } alt="image cap"/>
+              <div class="card-body d-flex flex-column">
+                <h3 class='card-text card-title product-title'>${
+                  products[i].title
+                }</h3>
+                <p class= "card-text price product-price"> £${products[
+                  i
+                ].price.toFixed(2)}</p>
+                <p class= "card-text">${products[i].description}</p>
+                <button type='button' class=' mt-auto btn-success btn add-to-basket'>Add to basket</button>
+              </div>
+            </div>
+         </div>`
+      );
+    }
+  });
+
+  $(".product-img").css({
+    margin: "10% auto",
+    height: "50%",
+    width: "60%",
+    "object-fit": "fit",
+  });
+
+  $(".card").css({ margin: "3%", "border-bottom": "none" });
+
+  $(".btn").css({ width: "70%", margin: "0 auto" });
+
+  $(".add-to-basket").on("click", function () {
+    var productTitle = $(this).siblings(".product-title").text();
+    var productPrice = $(this).siblings(".product-price").text().slice(2);
+    console.log("productPrice....", typeof productPrice);
+    var productPriceFloat = parseFloat(productPrice);
+    console.log("productPrice....", typeof productPriceFloat);
+    addtoBasket(productTitle, productPrice);
+
+    // Function to add the product to a basket, and send to localStorage
+    function addtoBasket(productTitle, productPrice) {
+      // Create a basket
+      var basketArray = [];
+      // Create a basket item
+      var basketItem = {
+        product: productTitle,
+        price: productPrice,
+      };
+
+      // Create my object that we push to localStorage
+      // If a basket is empty, it won't be in local storage
+      if (localStorage.getItem("basket") === null) {
+        // put the item in the basket
+        basketArray.push(basketItem);
+        // send the basket to localStorage
+        localStorage.setItem("basket", JSON.stringify(basketArray));
+      } else {
+        //  the basket is already in localStorage, basketArray becomes the basket in the localStorage using parse
+        basketArray = JSON.parse(localStorage.getItem("basket"));
+        // put the new item in the basket
+        basketArray.push(basketItem);
+        // return the basket to localStorage
+        localStorage.setItem("basket", JSON.stringify(basketArray));
+      }
+    }
+  });
+}
+
+document
+  .getElementById("saveEmailButton")
+  .addEventListener("click", function () {
+    let emailInput = document.getElementById("emailInput").value;
+    localStorage.setItem("email", emailInput);
+  });
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,20 +1,60 @@
-// get the search icon - and add a class 'search'
-$("#navbar-icons").children().eq(1).addClass("search");
-// prepend with a text input search box
-$(".search").prepend("<input id='searchInput'></input>");
-// get the search icon img and add id 'search-icon
-$("#navbar-icons").children().eq(1).children().eq(1).attr("id", "search-icon");
-// add click event to search icon img to show/hide search bar
-$("#search-icon").on("click", function () {
-  $("#searchInput").toggle();
-});
+// Create a search bar in the navbar to the right of the search icon
+function createSearchBar() {
+  // get the search icon - and add a class 'search'
+  $("#navbar-icons").children().eq(1).addClass("search");
+  // prepend with a text input search box
+  $(".search").prepend("<input id='searchInput'></input>");
+  // get the search icon img and add id 'search-icon
+  $("#navbar-icons")
+    .children()
+    .eq(1)
+    .children()
+    .eq(1)
+    .attr("id", "search-icon");
+  // add click event to search icon img to show/hide search bar
+  $("#search-icon").on("click", function () {
+    $("#searchInput").toggle();
+  });
+}
 
-// TODO: Create a smooth animation
+// Compare the user input to each product title and description, if it matches, push the product to an array
+// And send the array to the displayProductCards function
+function searchInput(product) {
+  $("#searchInput").on("input", function (event) {
+    $(".product-container").empty();
+    var filteredlist = [];
+    var searchInputString = event.target.value;
+    for (var i = 0; i < product.length; i++) {
+      searchInputString = searchInputString.toLowerCase();
+      if (
+        product[i].title.toLowerCase().includes(searchInputString) ||
+        product[i].description.toLowerCase().includes(searchInputString)
+      ) {
+        filteredlist.push(product[i]);
+      }
+    }
+    displayProductCards(filteredlist);
+  });
+}
 
-// When a user does a search console.log(search), when enter is pressed
-$("#searchInput").on("keypress", function (event) {
-  if (event.key === "Enter") {
-    console.log($("#searchInput").val());
-    console.log(getAllProducts());
-  }
-});
+// Get the category from the drop down menu click
+$(".category-dropdown")
+  .children()
+  .on("click", (e) => {
+    catergory = e.target.text;
+    getProductCatergory(catergory);
+  });
+
+// Add the category to the api url call
+function getProductCatergory(catergory) {
+  $(".product-container").empty();
+  var queryURL = "https://fakestoreapi.com/products/category/" + catergory;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    // get the data for that category
+  }).then(function (productData) {
+    // display the product cards for the category
+    displayProductCards(productData);
+  });
+}
